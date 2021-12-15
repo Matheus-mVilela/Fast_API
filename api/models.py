84551951e6f36233e7d028e5cc180d317063e74c
Matheus-mVilela <matheus_vilela@icloud.com>
2021-12-15 14:44:36 -0300

    docker migrate

--- api/models.py
+++ api/models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+import database
 
-class Users(Base):
-    
-    __tablename__ = 'users'
+
+class Users(database.Base):
+
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
